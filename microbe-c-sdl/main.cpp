2bf4aa9b1b349c3df14115a797d9855771b73386
Joel Longanecker <joel.longanecker@gmail.com>
2024-02-14 16:43:23 -0600

    even more progress!

--- microbe-c-sdl/main.cpp
+++ microbe-c-sdl/main.cpp
@@ -11,12 +11,34 @@ SDL_Surface *microbe_framebuffer_cache;
 SDL_Surface *microbe_backBuffer_cache;
 
 
-byte_t microbe_tiles[256*8];
-byte_t microbe_vram[32*32];
+byte_t microbe_tiles[256][64];
+byte_t microbe_vram[32][32];
 
-SDL_Color palette[256*4];  // 256 entries * 4 colors * 4 bytes for each color.
+SDL_Color palette[256][4];  // 256 entries * 4 colors * 4 bytes for each color.
 
-duk_ret_t setTile(duk_context *ctx){return 1;}
+bool isDirty = false;
+
+duk_ret_t setTile(duk_context *ctx){
+
+    int index = duk_require_int(ctx,0);
+
+    if(!duk_is_array(ctx,1)){
+        return DUK_RET_TYPE_ERROR;
+    }
+
+    if(duk_get_length(ctx, 1) != 64){
+        return DUK_ERR_ERROR;
+    }
+
+    for(int i = 0; i < 64;i++){
+            duk_get_prop_index(ctx, 1,i);
+        microbe_tiles[index][i]=duk_get_int(ctx,-1);
+    }
+
+    isDirty = true;
+
+    return 1;
+}
 duk_ret_t setVram(duk_context *ctx){return 1;}
 duk_ret_t setScroll(duk_context *ctx){return 1;}
 duk_ret_t setSprite(duk_context *ctx){return 1;}
@@ -31,6 +53,15 @@ void initDuktape(){
 
     duk_push_c_function(ctx, setTile, 2);
     duk_put_global_string(ctx, "setTile");
+
+    duk_push_c_function(ctx, setVram, 3);
+    duk_put_global_string(ctx, "setVram");
+
+    duk_push_c_function(ctx, setScroll, 2);
+    duk_put_global_string(ctx, "setScroll");
+
+    duk_push_c_function(ctx, setScroll, 2);
+    duk_put_global_string(ctx, "setScroll");
 
 }
 
