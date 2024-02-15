cb27c8d0f28d1e8e170626139e1814608ee62027
Joel Longanecker <joel.longanecker@gmail.com>
2024-02-14 23:10:09 -0600

    started setVram method

--- microbe-c-sdl/main.cpp
+++ microbe-c-sdl/main.cpp
@@ -4,50 +4,43 @@
 
 duk_context *ctx;
 
-
 void cleanDuktape()
 {
     duk_destroy_heap(ctx);
 }
 
-int main( int argc, char * argv[] )
+int main(int argc, char *argv[])
 {
-    SDL_Init( SDL_INIT_VIDEO | SDL_INIT_TIMER |SDL_INIT_AUDIO);
-
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_AUDIO);
 
     SDL_Window *window;
     SDL_Surface *screenSurface;
 
-    ctx=duk_create_heap_default();
-
+    ctx = duk_create_heap_default();
 
     initDuktapeGraphics(ctx);
 
-    window =initGraphics();
+    window = initGraphics();
 
     SDL_Event event;
 
     screenSurface = SDL_GetWindowSurface(window);
 
-    while(true)
+    while (true)
     {
         SDL_PollEvent(&event);
 
-        if(event.type == SDL_QUIT)
+        if (event.type == SDL_QUIT)
         {
             break;
         }
 
-
-        SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255,128,255));
-
+        SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255, 128, 255));
 
         SDL_UpdateWindowSurface(window);
-
     }
 
     cleanDuktape();
-
 
     return 0;
 }
--- microbe-c-sdl/microbe-graphics.cpp
+++ microbe-c-sdl/microbe-graphics.cpp
@@ -14,86 +14,73 @@ byte_t microbe_vram[32][32];
 
 byte_t microbe_tilePalette[256];
 
-SDL_Color microbe_palette[MAX_TILES][4];  // 256 entries * 4 colors * 4 bytes for each color.
+SDL_Color microbe_palette[MAX_TILES][4]; // 256 entries * 4 colors * 4 bytes for each color.
 
 bool isDirty = false;
 
 /** begin private methods **/
 
-
-
-
-
 /** end private methods **/
 
-
-
-
-SDL_Window * initGraphics()
+SDL_Window *initGraphics()
 {
 
- microbe_window = SDL_CreateWindow("microbe",-1,-1,-1,-1,SDL_WINDOW_RESIZABLE|SDL_WINDOW_SHOWN);
-    SDL_CreateWindowAndRenderer(640,480, SDL_WINDOW_RESIZABLE, &microbe_window, &microbe_renderer);
+    microbe_window = SDL_CreateWindow("microbe", -1, -1, -1, -1, SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN);
+    SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &microbe_window, &microbe_renderer);
 
-
-/*
-    for(int i = 0; i < MAX_TILES; i++)
+    for (int i = 0; i < MAX_TILES; i++)
     {
         microbe_tilePalette[i] = i;
-        microbe_tiles_cache[i] = SDL_CreateRGBSurfaceWithFormat(0,8,8,32,SDL_PIXELFORMAT_RGBA8888);
+        microbe_tiles_cache[i] = SDL_CreateRGBSurfaceWithFormat(0, 8, 8, 32, SDL_PIXELFORMAT_RGBA8888);
 
-        microbe_palette[i][0].a=0;
-        microbe_palette[i][0].r=0;
-        microbe_palette[i][0].g=0;
-        microbe_palette[i][0].b=0;
+        microbe_palette[i][0].a = 0;
+        microbe_palette[i][0].r = 0;
+        microbe_palette[i][0].g = 0;
+        microbe_palette[i][0].b = 0;
 
-        microbe_palette[i][1].a=255;
-        microbe_palette[i][1].r=0;
-        microbe_palette[i][1].g=0;
-        microbe_palette[i][1].b=0;
+        microbe_palette[i][1].a = 255;
+        microbe_palette[i][1].r = 0;
+        microbe_palette[i][1].g = 0;
+        microbe_palette[i][1].b = 0;
 
-        microbe_palette[i][2].a=255;
-        microbe_palette[i][2].r=64;
-        microbe_palette[i][2].g=64;
-        microbe_palette[i][2].b=64;
+        microbe_palette[i][2].a = 255;
+        microbe_palette[i][2].r = 64;
+        microbe_palette[i][2].g = 64;
+        microbe_palette[i][2].b = 64;
 
-        microbe_palette[i][3].a=255;
-        microbe_palette[i][3].r=128;
-        microbe_palette[i][3].g=128;
-        microbe_palette[i][3].b=128;
+        microbe_palette[i][3].a = 255;
+        microbe_palette[i][3].r = 128;
+        microbe_palette[i][3].g = 128;
+        microbe_palette[i][3].b = 128;
     }
-*/
+
     isDirty = true;
 
-return microbe_window;
+    return microbe_window;
 }
-
 
 duk_ret_t setTile(duk_context *ctx)
 {
 
-    int index = duk_require_int(ctx,0);
+    int index = duk_require_int(ctx, 0);
 
-    if(!duk_is_array(ctx,1))
+    if (!duk_is_array(ctx, 1))
     {
         return DUK_RET_TYPE_ERROR;
     }
 
-    if(duk_get_length(ctx, 1) != 64)
+    if (duk_get_length(ctx, 1) != 64)
     {
         return DUK_ERR_ERROR;
     }
 
-
-    for(int i = 0; i < 64; i++)
+    for (int i = 0; i < 64; i++)
     {
-        duk_get_prop_index(ctx, 1,i);
-        microbe_tiles[index][i]=duk_get_int(ctx,-1);
-        SDL_Color *pixels = ((SDL_Color*)microbe_tiles_cache[index]->pixels);
-        memccpy(pixels, &microbe_palette[ microbe_tilePalette[i]], 1, sizeof(SDL_Color));
+        duk_get_prop_index(ctx, 1, i);
+        microbe_tiles[index][i] = duk_get_int(ctx, -1);
+        SDL_Color *pixels = ((SDL_Color *)microbe_tiles_cache[index]->pixels);
+        memccpy(pixels, &microbe_palette[microbe_tilePalette[i]], 1, sizeof(SDL_Color));
     }
-
-
 
     isDirty = true;
 
@@ -101,6 +88,11 @@ duk_ret_t setTile(duk_context *ctx)
 }
 duk_ret_t setVram(duk_context *ctx)
 {
+    int x = duk_require_int(ctx,0);
+    int y = duk_require_int(ctx,1);
+    int tileIndex = duk_require_int(ctx,2);
+
+
     return 1;
 }
 duk_ret_t setScroll(duk_context *ctx)
@@ -116,7 +108,6 @@ duk_ret_t getPalette(duk_context *ctx)
     return 1;
 }
 
-
 void initDuktapeGraphics(duk_context *ctx)
 {
 
@@ -131,5 +122,4 @@ void initDuktapeGraphics(duk_context *ctx)
 
     duk_push_c_function(ctx, setScroll, 2);
     duk_put_global_string(ctx, "setScroll");
-
 }
