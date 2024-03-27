ff9897d13abc8a333bbb8d5d3d4fc809c9ee4cd7
Joel Longanecker <joel.longanecker@gmail.com>
2024-03-27 09:32:53 -0500

    pixel paintin

---
+++ Microbe.Engine/Components/PixelPainterComponent.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Microbe.Engine.Components
+{
+    partial class PixelPainterComponent
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.PickPencilButton = new System.Windows.Forms.Button();
+            this.LineButton = new System.Windows.Forms.Button();
+            this.RectButton = new System.Windows.Forms.Button();
+            this.CircleButton = new System.Windows.Forms.Button();
+            this.EraseButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.ClearColorButton = new System.Windows.Forms.Button();
+            this.Color1 = new System.Windows.Forms.Button();
+            this.Color2 = new System.Windows.Forms.Button();
+            this.Color3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 128F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1190, 772);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.PickPencilButton, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.LineButton, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.RectButton, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.CircleButton, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.EraseButton, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox1, 0, 6);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 5);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox2, 0, 7);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 9;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 96F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 96F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 96F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(122, 766);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // PickPencilButton
+            // 
+            this.PickPencilButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PickPencilButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PickPencilButton.Location = new System.Drawing.Point(3, 3);
+            this.PickPencilButton.Name = "PickPencilButton";
+            this.PickPencilButton.Size = new System.Drawing.Size(116, 42);
+            this.PickPencilButton.TabIndex = 0;
+            this.PickPencilButton.Text = "✏️";
+            this.PickPencilButton.UseVisualStyleBackColor = true;
+            // 
+            // LineButton
+            // 
+            this.LineButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LineButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LineButton.Location = new System.Drawing.Point(3, 51);
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(116, 42);
+            this.LineButton.TabIndex = 1;
+            this.LineButton.Text = "📏";
+            this.LineButton.UseVisualStyleBackColor = true;
+            // 
+            // RectButton
+            // 
+            this.RectButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RectButton.Location = new System.Drawing.Point(3, 99);
+            this.RectButton.Name = "RectButton";
+            this.RectButton.Size = new System.Drawing.Size(116, 42);
+            this.RectButton.TabIndex = 2;
+            this.RectButton.Text = "⏹️";
+            this.RectButton.UseVisualStyleBackColor = true;
+            // 
+            // CircleButton
+            // 
+            this.CircleButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CircleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CircleButton.Location = new System.Drawing.Point(3, 147);
+            this.CircleButton.Name = "CircleButton";
+            this.CircleButton.Size = new System.Drawing.Size(116, 42);
+            this.CircleButton.TabIndex = 3;
+            this.CircleButton.Text = "⭕";
+            this.CircleButton.UseVisualStyleBackColor = true;
+            // 
+            // EraseButton
+            // 
+            this.EraseButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EraseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EraseButton.Location = new System.Drawing.Point(3, 195);
+            this.EraseButton.Name = "EraseButton";
+            this.EraseButton.Size = new System.Drawing.Size(116, 42);
+            this.EraseButton.TabIndex = 4;
+            this.EraseButton.Text = "🚮";
+            this.EraseButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 291);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(116, 90);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fill Mode";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(84, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Outline";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 56);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Fill";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 42);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "🥫";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(131, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1056, 766);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 800);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tableLayoutPanel3);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 387);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(116, 90);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Color";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.ClearColorButton, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.Color1, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.Color2, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.Color3, 1, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 22);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(110, 65);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // ClearColorButton
+            // 
+            this.ClearColorButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ClearColorButton.Location = new System.Drawing.Point(3, 3);
+            this.ClearColorButton.Name = "ClearColorButton";
+            this.ClearColorButton.Size = new System.Drawing.Size(49, 26);
+            this.ClearColorButton.TabIndex = 0;
+            this.ClearColorButton.Text = "Clr";
+            this.ClearColorButton.UseVisualStyleBackColor = true;
+            // 
+            // Color1
+            // 
+            this.Color1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Color1.Location = new System.Drawing.Point(58, 3);
+            this.Color1.Name = "Color1";
+            this.Color1.Size = new System.Drawing.Size(49, 26);
+            this.Color1.TabIndex = 1;
+            this.Color1.Text = "button3";
+            this.Color1.UseVisualStyleBackColor = true;
+            // 
+            // Color2
+            // 
+            this.Color2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Color2.Location = new System.Drawing.Point(3, 35);
+            this.Color2.Name = "Color2";
+            this.Color2.Size = new System.Drawing.Size(49, 27);
+            this.Color2.TabIndex = 2;
+            this.Color2.Text = "button4";
+            this.Color2.UseVisualStyleBackColor = true;
+            // 
+            // Color3
+            // 
+            this.Color3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Color3.Location = new System.Drawing.Point(58, 35);
+            this.Color3.Name = "Color3";
+            this.Color3.Size = new System.Drawing.Size(49, 27);
+            this.Color3.TabIndex = 3;
+            this.Color3.Text = "button5";
+            this.Color3.UseVisualStyleBackColor = true;
+            // 
+            // PixelPainterComponent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "PixelPainterComponent";
+            this.Size = new System.Drawing.Size(1190, 772);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button PickPencilButton;
+        private System.Windows.Forms.Button LineButton;
+        private System.Windows.Forms.Button RectButton;
+        private System.Windows.Forms.Button CircleButton;
+        private System.Windows.Forms.Button EraseButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button ClearColorButton;
+        private System.Windows.Forms.Button Color1;
+        private System.Windows.Forms.Button Color2;
+        private System.Windows.Forms.Button Color3;
+    }
+}
---
+++ Microbe.Engine/IMicrobeAudio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Microbe.Engine
+{
+    public interface IMicrobeAudio
+    {
+        List<string> Notes { get; }
+        Sample[] Samples { get; set; }
+
+        void Deserialize(string fileName);
+        void PlayEffect(int sampleId);
+        void PlayMusic(int sampleId);
+        void SetSample(int index, int intervalMS, SampleSegment[] musicSegments);
+        void SetSampleRaw(int index, byte[] sampleData);
+        void StopMusic();
+    }
+}
