e5ed320d6625a9e1e75e82531fc974b68cd892c4
Joel Longanecker <joel.longanecker@gmail.com>
2024-04-02 22:31:16 -0500

    getting started

---
+++ Microbe.Player/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+
+public class MicrobeGame : Game { 
+
+    public MicrobeGame() {
+        // Set up the game window
+        var graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+    }
+
+    protected override void Initialize() {
+        base.Initialize();
+    }
+
+    protected override void LoadContent() {
+    }
+
+    protected override void UnloadContent() {
+    }
+
+    protected override void Update(GameTime gameTime) {
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime) {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        base.Draw(gameTime);
+    }
+
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        using (var game = new MicrobeGame()) {
+            game.Run();
+        }
+    }
+}
